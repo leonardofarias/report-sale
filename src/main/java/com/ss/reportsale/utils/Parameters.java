7338e8826e8ba39b9ea8613db028b14b57677de9
leofaria1806 <leonardo.faria@southsystem.com.br>
2019-07-23 16:43:06 -0300

    ajustes

--- src/main/java/com/ss/reportsale/utils/Parameters.java
+++ src/main/java/com/ss/reportsale/utils/Parameters.java
@@ -2,7 +2,11 @@ package com.ss.reportsale.utils;
 
 public class Parameters {
 
-  public static final String PATH_IN = System.getProperty("user.home") + "\\data\\in";
-  public static final String PATH_OUT = System.getProperty("user.home") + "\\data\\out";
+  public static final String PATH_IN = System.getProperty("os.name").toLowerCase().contains("win")
+          ? System.getProperty("user.home") + "\\data\\in"
+          : System.getProperty("user.home") + "/data/in";
+  public static final String PATH_OUT = System.getProperty("os.name").toLowerCase().contains("win")
+          ? System.getProperty("user.home") + "\\data\\out"
+          : System.getProperty("user.home") + "/data/out";
   public static final String SPLIT_VAR = "ç";
 }
